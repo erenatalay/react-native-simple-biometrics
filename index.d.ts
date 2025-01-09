dd4f654e9ab7e296160b82369f669a0614663e5c
erenatalay <ahmetatalay95@gmail.com>
2025-01-09 09:21:57 +0300

    feat:package complate

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,17 @@
-import type { BiometricType } from './src';
+import { BiometricTypeEnums } from './src/biometrics.types';
 
+enum BiometricEnums {
+  FaceID = 'FaceID',
+  TouchID = 'TouchID',
+  Passcode = 'Passcode',
+  None = 'None',
+}
 declare module 'react-native-simple-biometrics' {
+  export type BiometricType = keyof typeof BiometricTypeEnums;
+
   export function checkAvailability(): Promise<BiometricType>;
 
   export function authenticate(): Promise<boolean>;
 
-  export * from './src/biometrics.types';
+  export { BiometricEnums };
 }
